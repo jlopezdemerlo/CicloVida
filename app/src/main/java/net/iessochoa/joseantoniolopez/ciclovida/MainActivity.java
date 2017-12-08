6478e1aa980fe3405e33b24452f5f70cdba5ab53
JoseA <lmj@iesseveroochoa.net>
2017-12-08 11:40:44 +0100

    2017

--- app/src/main/java/net/iessochoa/joseantoniolopez/ciclovida/MainActivity.java
+++ app/src/main/java/net/iessochoa/joseantoniolopez/ciclovida/MainActivity.java
@@ -6,9 +6,10 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
-import android.support.v7.app.ActionBarActivity;
+
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -23,16 +24,16 @@ import android.widget.TextView;
 import java.util.ArrayList;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
 
     private final static String CICLO_TAG = "EstadoCV-Activity";
     private String ciclo_vida;
     private TextView txv_estado;
     private ArrayList<CharSequence> datosSpinner;
-    private ArrayAdapter<CharSequence>  adaptador;
+    private ArrayAdapter<CharSequence> adaptador;
     private Spinner cmbDesaparece;
     private CheckBox chkGuardar;
-    private int totalFragmento=0;
+    private int totalFragmento = 0;
 
     //pivate frgFragment CicloVidaFragment;
 
@@ -42,10 +43,10 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
 
 
-        Button btn_iniciar = (Button)findViewById(R.id.btn_iniciar_activity);
-        Button btn_finalizar = (Button)findViewById(R.id.btn_finalizar);
-        txv_estado = (TextView)findViewById(R.id.txv_estado);
-        chkGuardar=(CheckBox) findViewById(R.id.chkGuardar);
+        Button btn_iniciar = (Button) findViewById(R.id.btn_iniciar_activity);
+        Button btn_finalizar = (Button) findViewById(R.id.btn_finalizar);
+        txv_estado = (TextView) findViewById(R.id.txv_estado);
+        chkGuardar = (CheckBox) findViewById(R.id.chkGuardar);
 
         //frgFragment=(CicloVidaFragment)findViewById(R.id.frgFragment);
 
@@ -55,20 +56,19 @@ public class MainActivity extends ActionBarActivity {
         android.util.Log.i(CICLO_TAG, ciclo_vida);
 
         //Prueba Girar pantalla
-        cmbDesaparece=(Spinner) findViewById(R.id.cmbDesaparece);
-        if(savedInstanceState==null)//Primer Create
+        cmbDesaparece = (Spinner) findViewById(R.id.cmbDesaparece);
+        if (savedInstanceState == null)//Primer Create
         {
 
             datosSpinner = new ArrayList<CharSequence>();
             datosSpinner.add("Cero");
 
-        }else{//Recuperamos los datos del spinner si los hemos guardado
-            datosSpinner=savedInstanceState.getCharSequenceArrayList("DATOS_SPINNER");
-            if(datosSpinner==null){//no se guardaron los datos
+        } else {//Recuperamos los datos del spinner si los hemos guardado
+            datosSpinner = savedInstanceState.getCharSequenceArrayList("DATOS_SPINNER");
+            if (datosSpinner == null) {//no se guardaron los datos
                 datosSpinner = new ArrayList<CharSequence>();
                 datosSpinner.add("Cero");
-            }
-            else
+            } else
                 android.util.Log.i(CICLO_TAG, "Datos de Spinner recuperados en onCreate");
 
         }
@@ -77,19 +77,18 @@ public class MainActivity extends ActionBarActivity {
         cmbDesaparece.setAdapter(adaptador);
 
 
-
     }
 
     @Override
-    protected void onStart(){
+    protected void onStart() {
         super.onStart();
         ciclo_vida = "Estado: onStart";
         txv_estado.setText(ciclo_vida);
-        android.util.Log.i(CICLO_TAG,ciclo_vida);
+        android.util.Log.i(CICLO_TAG, ciclo_vida);
     }
 
     @Override
-    protected void onRestart(){
+    protected void onRestart() {
         super.onRestart();
         ciclo_vida = "Estado: onRestart";
         txv_estado.setText(ciclo_vida);
@@ -97,45 +96,46 @@ public class MainActivity extends ActionBarActivity {
     }
 
     @Override
-    protected void onResume(){
+    protected void onResume() {
         super.onResume();
         ciclo_vida = "Estado: onResume";
         txv_estado.setText(ciclo_vida);
-        android.util.Log.i(CICLO_TAG,ciclo_vida);
+        android.util.Log.i(CICLO_TAG, ciclo_vida);
     }
 
     @Override
-    protected void onPause(){
+    protected void onPause() {
         super.onPause();
         ciclo_vida = "Estado: onPause";
         txv_estado.setText(ciclo_vida);
-        android.util.Log.i(CICLO_TAG,ciclo_vida);
+        android.util.Log.i(CICLO_TAG, ciclo_vida);
     }
 
     @Override
-    protected void onStop(){
+    protected void onStop() {
         super.onStop();
         ciclo_vida = "Estado: onStop";
         txv_estado.setText(ciclo_vida);
-        android.util.Log.i(CICLO_TAG,ciclo_vida);
+        android.util.Log.i(CICLO_TAG, ciclo_vida);
     }
 
     @Override
-    protected void onDestroy(){
+    protected void onDestroy() {
         super.onDestroy();
         ciclo_vida = "Estado: onDestroy";
         txv_estado.setText(ciclo_vida);
-        android.util.Log.i(CICLO_TAG,ciclo_vida);
+        android.util.Log.i(CICLO_TAG, ciclo_vida);
     }
+
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
         ciclo_vida = "Estado: onSaveInstanceState-Guardar los datos que quieras recuperar de  reconstruccion  Activity";
         txv_estado.setText(ciclo_vida);
         android.util.Log.i(CICLO_TAG, ciclo_vida);
-        if(chkGuardar.isChecked()){
+        if (chkGuardar.isChecked()) {
             android.util.Log.i(CICLO_TAG, "Guardamos los datos del Spinner");
-            outState.putCharSequenceArrayList("DATOS_SPINNER",datosSpinner);
+            outState.putCharSequenceArrayList("DATOS_SPINNER", datosSpinner);
         }
 
     }
@@ -171,16 +171,17 @@ public class MainActivity extends ActionBarActivity {
     }
 
 
-    public void iniciarActivity(View view){
+    public void iniciarActivity(View view) {
         Intent starter = new Intent(this, SegundaActivity.class);
         startActivity(starter);
     }
 
-    public void finalizarActivity(View view){
+    public void finalizarActivity(View view) {
         finish();
 
     }
-    public void cuadroDialogo(View v){
+
+    public void cuadroDialogo(View v) {
         final AlertDialog.Builder dialogo1 = new AlertDialog.Builder(this);
         dialogo1.setTitle("Aviso");
         dialogo1.setMessage("El cuadro de dialogo es un fragment y la Activity no cambia de estado");
@@ -196,15 +197,18 @@ public class MainActivity extends ActionBarActivity {
 
         dialogo1.show();
     }
-    public void iniciarActivityVisible(View view){
+
+    public void iniciarActivityVisible(View view) {
         Intent starter = new Intent(this, AcercaDeActivity.class);
         startActivity(starter);
     }
-    public void verCicloVida(View v){
+
+    public void verCicloVida(View v) {
         Intent starter = new Intent(this, ImgenCicloVidaActivity.class);
         startActivity(starter);
     }
-    public void cargarDatosSpinner(View v){
+
+    public void cargarDatosSpinner(View v) {
         datosSpinner.add("uno");
         datosSpinner.add("dos");
         datosSpinner.add("tres");
@@ -215,46 +219,49 @@ public class MainActivity extends ActionBarActivity {
 
 
     }
-    void asignaParametrosContenedorFragment(View v){
-        FrameLayout.LayoutParams params=
+
+    void asignaParametrosContenedorFragment(View v) {
+        FrameLayout.LayoutParams params =
                 new FrameLayout.LayoutParams(
                         FrameLayout.LayoutParams.MATCH_PARENT,
                         FrameLayout.LayoutParams.MATCH_PARENT);
         v.setLayoutParams(params);
 
     }
-    public void cargaFragment(View v){
+
+    public void cargaFragment(View v) {
 
         FragmentManager fragmentManager;
         FragmentTransaction transaction;
         fragmentManager = getSupportFragmentManager();
         transaction = fragmentManager.beginTransaction();
-        CicloVidaFragment frgFragment=CicloVidaFragment.newInstance(Integer.toString(totalFragmento));
+        CicloVidaFragment frgFragment = CicloVidaFragment.newInstance(Integer.toString(totalFragmento));
         transaction.replace(R.id.lytFragment, frgFragment);
         transaction.addToBackStack(null);
         transaction.commit();
         fragmentManager.executePendingTransactions();
-        ((TextView)(findViewById(R.id.lblFragmentDinamico))).setVisibility(View.INVISIBLE);
+        ((TextView) (findViewById(R.id.lblFragmentDinamico))).setVisibility(View.INVISIBLE);
 
         totalFragmento++;
 
     }
-    public void informacion(View v){
 
-            final AlertDialog.Builder dialogo1 = new AlertDialog.Builder(this);
-            dialogo1.setTitle("Informacion");
-            dialogo1.setMessage("Para ver los estados ve a \"logcat\" y en \"loglevel\" elige \"info\". Escribe :\nEstadoCV: Todos los estados\nEstadoCV-A: Estados Activity\nEstadoCV-F: Estados Fragment ");
-            dialogo1.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialogo, int i) {
-                    //
-                    dialogo.cancel();
+    public void informacion(View v) {
+
+        final AlertDialog.Builder dialogo1 = new AlertDialog.Builder(this);
+        dialogo1.setTitle("Informacion");
+        dialogo1.setMessage("Para ver los estados ve a \"logcat\" y en \"loglevel\" elige \"info\". Escribe :\nEstadoCV: Todos los estados\nEstadoCV-A: Estados Activity\nEstadoCV-F: Estados Fragment ");
+        dialogo1.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogo, int i) {
+                //
+                dialogo.cancel();
 
 
-                }
-            });
+            }
+        });
 
-            dialogo1.show();
+        dialogo1.show();
 
     }
 }
